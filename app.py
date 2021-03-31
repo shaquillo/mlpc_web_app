c8350c9b8d7adb0a1e1e0cea1be62df3ed9897aa
shaquillo <chimishaquille@gmail.com>
2021-04-01 00:46:10 +0100

    Adding model to add subjects

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
