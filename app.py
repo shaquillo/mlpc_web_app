8171bf3a765db85e80fadfe9a8b0306beff61a53
shaquillo <chimishaquille@gmail.com>
2021-04-01 12:44:18 +0100

    Remained with printing results on the result page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
+from model import predict
+import logging
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -6,6 +8,11 @@ app.config.from_object('config')
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def prediction():
+    pred_result = predict.predict(request.form)
+    return render_template('result.html', pred_result=pred_result)
 
 if __name__ == '__main__':
     app.run()
